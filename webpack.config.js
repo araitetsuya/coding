4675e58f9992c0032f6a2bd0b5e6bb8364205dd5
araitetsuya <arai@valeur3.com>
2020-07-22 19:15:35 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: "production",
+
+    // メインのJS
+    entry: "./src/main.ts",
+    // 出力ファイル
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts']
+    }
+};
